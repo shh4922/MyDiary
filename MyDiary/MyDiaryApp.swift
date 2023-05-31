7fdb737bd1556398e1af396b2a789f6ae375363b
hyeonho <gusgh4922@gmail.com>
2023-05-31 21:32:21 +0900

    Initial Commit

---
+++ MyDiary/MyDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDiaryApp.swift
+//  MyDiary
+//
+//  Created by Hyeonho on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct MyDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
